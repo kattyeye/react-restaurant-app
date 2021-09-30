f823b6fab00c3f5d45ff0324261ee3489bec4f6e
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-30 16:07:50 -0400

    Adding menu items in an array

---
+++ src/UTILITIES/Menu.js
@@ -0,0 +1,74 @@
+const menuItems = [
+    {
+        id: 1,
+        name: 'Pepperoni Pizza',
+        description: 'it is what it is',
+        price: '$10'
+    },
+    {
+        id: 2,
+        name: 'Cheese Pizza',
+        description: 'it is what it is',
+        price: '$10'
+    },
+    {
+        id: 3,
+        name: 'Supreme Pizza',
+        description: 'it is what it is',
+        price: '$10'
+    },
+    {
+        id: 4,
+        name: 'Veggie Pizza',
+        description: 'it is what it is',
+        price: '$10'
+    },
+    {
+        id: 5,
+        name: 'Hawaiian Pizza',
+        description: 'it is what it is',
+        price: '$10'
+    },
+    {
+        id: 6,
+        name: 'Cinnamon Sticks',
+        description: 'it is what it is',
+        price: '$8'
+    },
+    {
+        id: 7,
+        name: 'Bread Sticks',
+        description: 'it is what it is',
+        price: '$8'
+    },
+    {
+        id: 8,
+        name: 'Cannoli (2)',
+        description: 'it is what it is',
+        price: '$4'
+    },
+    {
+        id: 9,
+        name: 'Like a Salad',
+        description: 'it is what it is',
+        price: '$6'
+    },
+    {
+        id: 10,
+        name: 'Caesar Salad',
+        description: 'it is what it is',
+        price: '$6'
+    },
+    {
+        id: 11,
+        name: 'Cream Soda',
+        description: 'it is what it is',
+        price: '$2'
+    },
+    {
+        id: 12,
+        name: 'Root Beer',
+        description: 'it is what it is',
+        price: '$2'
+    },
+]
